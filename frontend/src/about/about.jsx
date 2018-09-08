4a8cd0baf306779d54b9e4ca0cb4c19f87767ac0
MatzCouz <matheus.sousacoutinho@gmail.com>
2018-09-07 22:37:16 -0300

    Add all files

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa Historia</h2>
+        <p>Nois é foda</p>
+        <h2>Uhhuuuu</h2>
+        <p>Nois é foda pakas </p>
+    </div>
+)
